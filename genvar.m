0030ffe67eaa49971123ce45c8befa5820b1d4e5
joel <j.glemne@gmail.com>
2016-10-06 16:57:46 +1300

    functioning genvar

--- genvar.m
+++ genvar.m
@@ -1,10 +1,10 @@
-function variates = genvar(boolTrunc,eta,sigma,interval,nSamples,nVariates)
+function variates = genvar(boolTrunc,eta,sigma,interval,nSamples,nVariates,truncInterval)
 
 % Creating time interval
 nTime = linspace(-interval,interval,nSamples);
 
 % Generating density functions
-[cDensFun,tcDensFun] = cdfs(nTime,eta,sigma,-interval,interval);
+[cDensFun,tcDensFun] = cdfs(nTime,eta,sigma,-truncInterval,truncInterval);
 
 % Choosing to use continuous or truncated density function
 if boolTrunc
@@ -13,11 +13,13 @@ else
     densFun = cDensFun;
 end
 
+length(densFun)
+
 % Creating cumulative distribution function
 cdf = zeros(1,nSamples);
 sum = 0;
 for i=2:nSamples
-    sum = sum + (interval(i)-interval(i-1))*((densFun(i)+densFun(i-1))/2);
+    sum = sum + (nTime(i)-nTime(i-1))*((densFun(i)+densFun(i-1))/2);
     cdf(i) = sum;
 end
 
@@ -25,8 +27,8 @@ variates = zeros(1,nVariates);
 counter = 1;
 while counter <= nVariates
     % Get index for closest value
-    [~, i] = min(abs(df-rand(1)));
-    variates(counter) = interval(i);
+    [~, i] = min(abs(cdf-rand(1)));
+    variates(counter) = nTime(i);
     counter = counter + 1;
 end
 
